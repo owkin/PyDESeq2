df81a284d9995708bf13733b31e09d4ff29a53de
Boris MUZELLEC <boris.muzellec@owkin.com>
2022-12-19 11:12:19 +0100

    build: change pandas requirement to >= 1.3 to meet myBinder requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         "jupyter",
         "numpy>=1.23.0",
-        "pandas>=1.4.0",
+        "pandas>=1.3.0",
         "scikit-learn>=1.1.0",
         "scipy>=1.7.0",
         "statsmodels",
