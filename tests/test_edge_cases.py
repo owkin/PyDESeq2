cdf9a6ec0bbd6fbb0cb2a519200174ef619f1b52
Boris MUZELLEC <boris.muzellec@owkin.com>
2022-12-23 16:06:47 +0100

    test: add valid count matrix pytests

--- tests/test_edge_cases.py
+++ tests/test_edge_cases.py
@@ -3,15 +3,16 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import pytest
 
 import tests
 from pydeseq2.DeseqDataSet import DeseqDataSet
 from pydeseq2.DeseqStats import DeseqStats
 
 
-def test_zero_genes(tol=0.02):
+def test_zero_genes():
     """Test that genes with only 0 counts are handled by DeseqDataSet et DeseqStats,
-    and that NaNs are returned
+    and that NaNs are returned.
     """
 
     test_path = str(Path(os.path.realpath(tests.__file__)).parent.resolve())
@@ -48,3 +49,43 @@ def test_zero_genes(tol=0.02):
     assert res_df.loc[zero_genes].stat.isna().all()
     assert res_df.loc[zero_genes].pvalue.isna().all()
     assert res_df.loc[zero_genes].padj.isna().all()
+
+
+def test_nan_counts():
+    """Test that a ValueError is thrown when the count matrix contains NaNs."""
+    counts_df = pd.DataFrame({"gene1": [0, np.nan], "gene2": [4, 12]})
+    clinical_df = pd.DataFrame({"condition": [0, 1]})
+
+    with pytest.raises(ValueError):
+        DeseqDataSet(counts_df, clinical_df, design_factor="condition")
+
+
+def test_numeric_counts():
+    """Test that a ValueError is thrown when the count matrix contains
+    non-numeric values.
+    """
+    counts_df = pd.DataFrame({"gene1": [0, "a"], "gene2": [4, 12]})
+    clinical_df = pd.DataFrame({"condition": [0, 1]})
+
+    with pytest.raises(ValueError):
+        DeseqDataSet(counts_df, clinical_df, design_factor="condition")
+
+
+def test_integer_counts():
+    """Test that a ValueError is thrown when the count matrix contains
+    non-integer values."""
+    counts_df = pd.DataFrame({"gene1": [0, 1.5], "gene2": [4, 12]})
+    clinical_df = pd.DataFrame({"condition": [0, 1]})
+
+    with pytest.raises(ValueError):
+        DeseqDataSet(counts_df, clinical_df, design_factor="condition")
+
+
+def test_non_negative_counts():
+    """Test that a ValueError is thrown when the count matrix contains
+    negative values."""
+    counts_df = pd.DataFrame({"gene1": [0, -1], "gene2": [4, 12]})
+    clinical_df = pd.DataFrame({"condition": [0, 1]})
+
+    with pytest.raises(ValueError):
+        DeseqDataSet(counts_df, clinical_df, design_factor="condition")
