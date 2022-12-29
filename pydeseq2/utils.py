333ac951504a16b2050f8c9865f3f33ee99fd46e
Boris MUZELLEC <boris.muzellec@owkin.com>
2022-12-29 15:13:40 +0100

    docs: fix argument order

--- pydeseq2/utils.py
+++ pydeseq2/utils.py
@@ -194,11 +194,10 @@ def build_design_matrix(
         try:
             ref_level = design_matrix.pop(ref)
         except KeyError as e:
-            print(
+            raise e(
                 "The reference level must correspond to"
                 " one of the design factor values."
             )
-            raise e
         design_matrix.insert(1, ref, ref_level)
     if not expanded:  # drop last factor
         design_matrix.drop(columns=design_matrix.columns[-1], axis=1, inplace=True)
