147bce5a49cd8e78f87a7b9f6505e9305070062d
maikia <maria.telenczuk@owkin.com>
2023-01-06 12:39:16 +0100

    if the data directory for synthetic data does not exist loading directly from github
    (rebase and fix conflict with load_example_data refactoring)

--- pydeseq2/utils.py
+++ pydeseq2/utils.py
@@ -74,17 +74,29 @@ def load_example_data(
     datasets_path = Path(pydeseq2.__file__).parent.parent / "datasets"
 
     if dataset == "synthetic":
+
         path_to_data = datasets_path / "synthetic"
+        if Path(path_to_data).is_dir():
+            path_to_data_counts = path_to_data / "test_counts.csv"
+            path_to_data_clinical = path_to_data / "test_clinical.csv"
+        else:
+            # if the path does not exist (as is the case in RDT) load it from github
+            url_to_data = (
+                "https://raw.githubusercontent.com/owkin/"
+                "PyDESeq2/main/datasets/synthetic/"
+            )
+            path_to_data_counts = url_to_data + "/test_counts.csv"
+            path_to_data_clinical = url_to_data + "/test_clinical.csv"
 
         if modality == "raw_counts":
             df = pd.read_csv(
-                path_to_data / "test_counts.csv",
+                path_to_data_counts,
                 sep=",",
                 index_col=0,
             ).T
         elif modality == "clinical":
             df = pd.read_csv(
-                path_to_data / "test_clinical.csv",
+                path_to_data_clinical,
                 sep=",",
                 index_col=0,
             )
