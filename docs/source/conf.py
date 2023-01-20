5f088f7003f7032012686544f04a8d69eed55d8a
Boris MUZELLEC <boris.muzellec@owkin.com>
2023-01-20 10:56:43 +0100

    docs: ignore DomainWarnings from statsmodels in sphinx-gallery examples

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,13 +8,20 @@
 
 # -- Path setup --------------------------------------------------------------
 
+import warnings
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 from datetime import date
 
+from statsmodels.tools.sm_exceptions import DomainWarning
+
 import pydeseq2
+
+# Ignore DomainWarning raised by statsmodels when fitting a Gamma GLM with identity link.
+warnings.simplefilter("ignore", DomainWarning)
 
 # -- Project information -----------------------------------------------------
 
