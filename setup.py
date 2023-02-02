6579aca0fe482f4cd0c4613d0202d1db07a7d57a
illumination-k <illumination.k.27@gmail.com>
2023-02-02 15:23:24 +0900

    precommit

--- setup.py
+++ setup.py
@@ -37,6 +37,12 @@ setup(
         "matplotlib>=3.6.2",  # not sure why sphinx_gallery does not work without it
     ],  # external packages as dependencies
     extras_require={
-        "dev": ["pytest>=6.2.4", "pre-commit>=2.13.0", "numpydoc"],
+        "dev": [
+            "pytest>=6.2.4",
+            "pre-commit>=2.13.0",
+            "numpydoc",
+            "mypy",
+            "pandas-stub",
+        ],
     },
 )
