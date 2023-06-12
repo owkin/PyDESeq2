6ffdd1b3d4e50aefabe542531e0644a975d0e285
Boris MUZELLEC <boris.muzellec@owkin.com>
2023-06-12 10:19:07 +0200

    style: refactor the full rank warning

--- pydeseq2/dds.py
+++ pydeseq2/dds.py
@@ -1043,8 +1043,9 @@ class DeseqDataSet(ad.AnnData):
         if rank < num_vars:
             warnings.warn(
                 "The design matrix is not full rank, so the model cannot be "
-                "fitted. Please remove the design variables that are linear "
-                "combinations of others.",
+                "fitted, but some operations like design-free VST remain possible. "
+                "To perform differential expression analysis, please remove the design "
+                "variables that are linear combinations of others.",
                 UserWarning,
                 stacklevel=2,
             )
