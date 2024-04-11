cc09de70b92e4d880722739ef176a3c232d04532
jeandut <jeandut@hotmail.com>
2024-04-11 15:51:35 +0200

    Update examples/plot_pandas_io_example.py

--- examples/plot_pandas_io_example.py
+++ examples/plot_pandas_io_example.py
@@ -151,7 +151,6 @@ print(dds)
 # %%
 # As such, it can be saved using
 # `pickle.dump <https://docs.python.org/3/library/pickle.html#pickle.dump>`_.
-
 with open(os.path.join(OUTPUT_PATH, "dds.pkl"), "wb") as f:
     pkl.dump(dds, f)
 
